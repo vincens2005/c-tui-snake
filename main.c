d774826263a85059cb370ddee9b4e466777fedff
cukmekerb <cukmekerb@gmail.com>
2021-09-11 18:21:28 -0700

    snake movement system

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
+#define LEFT 0
+#define UP 1
+#define RIGHT 2
+#define DOWN 3
+
+int framerate = 10; 
 
 int game[1000][1000]; // if your terminal is over 1000 characters wide this will not work
 
@@ -10,15 +18,16 @@ int snake[1000][3];
 
 int apple_pos[2];
 
-int snake_len = 1;
+int snake_len = 8;
 
-int score = 0;
+int direction = LEFT;
 
 void setup_ncurses() {
 	initscr();
 	cbreak();
 	noecho();
 	clear();
+	nodelay(stdscr, TRUE);
 	curs_set(FALSE);
 	refresh();
 }
@@ -54,36 +63,79 @@ void generate_frame() {
 	}
 }
 
+void set_apple_pos() {
+	// random apple position
+	apple_pos[0] = rand_range(0, COLS - 2);
+	apple_pos[1] = rand_range(0, LINES);
+}
+
 void move_snake() {
 	int new_snake[1000][3];
-	for (int i = 0; i < 1000; i++) {
-		new_snake[i + 1][0] = snake[i][0];
-		new_snake[i + 1][1] = snake[i][1];
-		new_snake[i + 1][2] = snake[i][2];
+	
+	new_snake[0][0] = 1;
+	new_snake[0][1] = snake[0][1];
+	new_snake[0][2] = snake[0][2];
+	if (direction == RIGHT) {
+		new_snake[0][1] += 1;
+	}
+	else if (direction == LEFT) {
+		new_snake[0][1] -= 1;
+	}
+	else if (direction == DOWN) {
+		new_snake[0][2] += 1;
+	}
+	else if (direction == UP) {
+		new_snake[0][2] -= 1;
+	}
+	
+	// wrap snake
+	if (new_snake[0][1] < 0) {
+		new_snake[0][1] = COLS;
+	}
+	if (new_snake[0][1] > COLS) {
+		new_snake[0][1] = 0;
+	}
+	if (new_snake[0][2] == LINES) {
+		new_snake[0][2] = 0;
+	}
+	if (new_snake[0][2] < 0) {
+		new_snake[0][2] = LINES - 1;
+	}
+	
+	for (int i = 0; i < 999; i++) {
 		if (!snake[i][0]) {
 			break;
 		}
+		new_snake[i + 1][0] = snake[i][0];
+		new_snake[i + 1][1] = snake[i][1];
+		new_snake[i + 1][2] = snake[i][2];
 	}
-	int snake = new_snake;
+	
+	for (int i = 0; i < 1000; i++) {
+		// snake[i][0] = new_snake[i][0];
+		snake[i][1] = new_snake[i][1];
+		snake[i][2] = new_snake[i][2];
+	}
+}
+
+void build_snake() {
+	for (int i = 0; i < snake_len; i++) {
+		snake[i][0] = 1;
+		snake[i][1] = COLS / 2 + i;
+		snake[i][2] = LINES / 2;
+	}
 }
 
 int main() {
 	setup_ncurses();
-	game[50][10] = 1;
-	draw_frame();
-	sleep(1);
-	
-	snake[0][0] = 1;
-	snake[0][1] = 15;
-	snake[0][2] = 3;
-	
-	// random apple position
-	apple_pos[0] = rand_range(0, COLS - 2);
-	apple_pos[1] = rand_range(0, LINES);
-	
-	generate_frame();
-	draw_frame();
-	sleep(2);
+	build_snake();
+	set_apple_pos();
+	while (1) {
+		move_snake();
+		generate_frame();
+		draw_frame();
+		usleep(100000);
+	}
 	endwin();
 	printf("hellow world\r\n");
 	printf("%d\r\n", rand_range(0, 10));
